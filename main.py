dd4db01b2b1e5adf7f08d90ffb6ed6ff138796ca
jonathanAnguise <jonathan.anguise@protonmail.com>
2022-07-14 00:18:56 +0200

    Update main find_the_minimum_quantity_of_permutations, checking incorrect input Add else block as good practice remove useless debugging lines
    Update test_functions by Creating unit tests for function3
    Reformat file
    add mock for directory for test_find_the_first_file_owner_admin_executable_lower_than_14_MB_valid
    remove useless import

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def find_the_first_file_owner_admin_executable_lower_than_14_MB(user_path):
     path = pathlib.Path(user_path)
     max_size = 14 * 2 ** 20  # 14MB
 
-    if path.exists() == False:
+    if not path.exists():
         return "Path not valid"
     for file in path.iterdir():
         # Don't check for directory
@@ -49,6 +49,7 @@ def find_the_first_file_owner_admin_executable_lower_than_14_MB(user_path):
             return file.name
     return "No file matching found"
 
+
 def find_the_minimum_quantity_of_permutations(sequence):
     """
     The function finds the minimum quantity of permutations so that the sequence ends interspersed.
@@ -57,35 +58,27 @@ def find_the_minimum_quantity_of_permutations(sequence):
     """
     nb_of_swat = 0
     # check if the sequence can be interspersed.
-    if abs(sequence.count(0) - sequence.count(1)) != 0 and len(sequence) % 2 == 0:
+    for i in sequence:
+        if i != 0 and i != 1:
+            return "not valid"
+    if len(sequence) == 0:
+        return "not valid"
+    elif abs(sequence.count(0) - sequence.count(1)) != 0 and len(sequence) % 2 == 0:
         return "not possible"
     elif abs(sequence.count(0) - sequence.count(1)) != 1 and len(sequence) % 2 != 0:
         return "not possible"
     # Start reading the sequence
-    for i in range(1, len(sequence)):  # Don't evaluate first element because it's not needed
-        if sequence[i] == sequence[i - 1]:
-            nb_of_swat += 1
-            # find the first element different and swap it
-            for j in range(i + 1, len(sequence)):
-                if sequence[i] != sequence[j]:
-                    temp = sequence[i]
-                    sequence[i] = sequence[j]
-                    sequence[j] = temp
-                    break
-                else:
-                    continue
-    return nb_of_swat
-
-
-seq1 = [0, 1, 1, 0, 0, 0, 1]
-seq2 = [0, 1]
-seq3 = [0, 0, 1]
-seq4 = [0, 0, 1, 1]
-seq5 = [1, 0, 1, 1]
-seq6 = [1, 0, 0, 1, 1]
-print(find_the_minimum_quantity_of_permutations(seq1))
-print(find_the_minimum_quantity_of_permutations(seq2))
-print(find_the_minimum_quantity_of_permutations(seq3))
-print(find_the_minimum_quantity_of_permutations(seq4))
-print(find_the_minimum_quantity_of_permutations(seq5))
-print(find_the_minimum_quantity_of_permutations(seq6))
+    else:
+        for i in range(1, len(sequence)):  # Don't evaluate first element because it's not needed
+            if sequence[i] == sequence[i - 1]:
+                nb_of_swat += 1
+                # find the first element different and swap it
+                for j in range(i + 1, len(sequence)):
+                    if sequence[i] != sequence[j]:
+                        temp = sequence[i]
+                        sequence[i] = sequence[j]
+                        sequence[j] = temp
+                        break
+                    else:
+                        continue
+        return nb_of_swat
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,10 +1,10 @@
-import pytest
 import pathlib
 from unittest.mock import patch
-from os import chown, chmod, mkdir, remove, rmdir
+
+import pytest
+
 import main as script
 
-PATH = "../temp_testing_dir/"
 MAX_SIZE = 14 * 2 ** 20
 NO_EXECUTION_CODE = 33204
 EXECUTION_USER_CODE = 33268
@@ -15,10 +15,13 @@ class Stat:
         self.st_size = size
         self.st_mode = mode
 
+
 class TestResource:
     # Function 1
     @pytest.mark.parametrize("input_1, input_2, output",
-                             [([1, 5, 2, 3, 4], [3, 8, 4], 3),
+                             [([8, 5, 2, 3, 4], [3, 8, 4], 3),
+                              ([3, 8, 4], [8, 5, 2, 3, 4], 3),
+                              ([], [], "No data matching found"),
                               ([1, 2, 3], [-3, 14, 6, 10], "No data matching found")])
     def test_find_first_repeated_number(self, input_1, input_2, output):
         assert script.find_first_repeated_number(input_1, input_2) == output
@@ -32,32 +35,50 @@ class TestResource:
     def test_find_the_first_file_owner_admin_executable_lower_than_14_MB_valid(self):
         with patch.object(pathlib.Path, 'owner') as mock_owner:
             with patch.object(pathlib.Path, 'stat') as mock_size:
-                mock_size.return_value = Stat(size=MAX_SIZE, mode=EXECUTION_USER_CODE)
-                mock_owner.return_value = "admin"
-                assert script.find_the_first_file_owner_admin_executable_lower_than_14_MB(".") == "TODO"
-
+                with patch.object(pathlib.Path, 'is_dir') as mock_dir:
+                    mock_size.return_value = Stat(size=MAX_SIZE, mode=EXECUTION_USER_CODE)
+                    mock_dir.return_value = False
+                    mock_owner.return_value = "admin"
+                    assert script.find_the_first_file_owner_admin_executable_lower_than_14_MB(".") == ".pytest_cache"
 
     def test_find_the_first_file_owner_admin_executable_lower_than_14_MB_size_upp(self):
         with patch.object(pathlib.Path, 'owner') as mock_owner:
             with patch.object(pathlib.Path, 'stat') as mock_size:
-                mock_size.return_value = Stat(size=MAX_SIZE+1, mode=EXECUTION_USER_CODE)
+                mock_size.return_value = Stat(size=MAX_SIZE + 1, mode=EXECUTION_USER_CODE)
                 mock_owner.return_value = "admin"
-                assert script.find_the_first_file_owner_admin_executable_lower_than_14_MB(".") == "No file matching found"
-
+                assert script.find_the_first_file_owner_admin_executable_lower_than_14_MB(
+                    ".") == "No file matching found"
 
     def test_find_the_first_file_owner_admin_executable_lower_than_14_MB_owner_not_admin(self):
         with patch.object(pathlib.Path, 'owner') as mock_owner:
             with patch.object(pathlib.Path, 'stat') as mock_size:
                 mock_size.return_value = Stat(size=MAX_SIZE, mode=EXECUTION_USER_CODE)
                 mock_owner.return_value = "not admin"
-                assert script.find_the_first_file_owner_admin_executable_lower_than_14_MB(".") == "No file matching found"
-
+                assert script.find_the_first_file_owner_admin_executable_lower_than_14_MB(
+                    ".") == "No file matching found"
 
     def test_find_the_first_file_owner_admin_executable_lower_than_14_MB_only_directories(self):
         with patch.object(pathlib.Path, 'owner') as mock_owner:
             with patch.object(pathlib.Path, 'stat') as mock_size:
                 with patch.object(pathlib.Path, 'is_dir') as mock_dir:
+                    mock_dir.return_value = True
                     mock_owner.return_value = True
                     mock_size.return_value = Stat(size=MAX_SIZE, mode=EXECUTION_USER_CODE)
                     mock_owner.return_value = "admin"
-                    assert script.find_the_first_file_owner_admin_executable_lower_than_14_MB(".") == "No file matching found"
+                    assert script.find_the_first_file_owner_admin_executable_lower_than_14_MB(
+                        ".") == "No file matching found"
+
+    # Function 3
+    @pytest.mark.parametrize("input_1, output", [
+        ([0, 1, 1, 0, 0, 0, 1], 2),
+        ([0, 1], 0),
+        ([0, 0, 1], 1),
+        ([0, 0, 1, 1], 1),
+        ([1, 0, 1, 1], "not possible"),
+        ([1, 0, 0, 1, 1], 1),
+        ([], "not valid"),
+        ([1, "1", 0], "not valid"),
+        ([1], 0),
+    ])
+    def test_find_the_minimum_quantity_of_permutations(self, input_1, output):
+        assert script.find_the_minimum_quantity_of_permutations(input_1) == output
